25e005dc153bc08a9fe204476eb9bbd0919b0c8c
不鸽的燕子 <118051943+bugedeyanzi@users.noreply.github.com>
2025-09-14 07:44:42 +0800

    一些代码的上传

---
+++ python重复（if）.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#赋值区域
+A = input("你冲q币吗？")
+B = ("冲" or "不冲" or "冲！") 
+C = ("")
+
+#判断区域
+if A == B:
+    print("给钱，快给钱！")
+else:
+    if A == C:
+        print("为什么不说话！")
+    else:
+        print("你的父母亖了！")
+    print("谢谢你的支持！")
+print("程序结束！")
